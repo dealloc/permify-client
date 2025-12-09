a3ee75f63496f9067af0ac1a5097770bd60a1976
Wannes Gennar <wannes.gennar@gmail.com>
2025-12-09 09:51:52 +0100

    feat: run Postgres for example Permify

--- examples/aspire/Permify.Client.AppHost/AppHost.cs
+++ examples/aspire/Permify.Client.AppHost/AppHost.cs
@@ -2,12 +2,19 @@ using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
+var postgres = builder.AddPostgres("postgres")
+    .WithArgs("-c", "track_commit_timestamp=on")
+    .AddDatabase("permify-db");
+
 var permify = builder.AddContainer("permify", "permify/permify")
     .WithImageTag("latest")
     .WithImageRegistry("ghcr.io")
-    .WithHttpEndpoint(targetPort: 3476, name: "http")
-    .WithHttpEndpoint(targetPort: 3478, name: "grpc")
-    .WithHttpHealthCheck("/healthz");
+    .WithHttpEndpoint(port: 3476, targetPort: 3476, name: "http")
+    .WithHttpEndpoint(port: 3478, targetPort: 3478, name: "grpc")
+    .WithHttpHealthCheck("/healthz")
+    .WithEnvironment("PERMIFY_DATABASE_ENGINE", "postgres")
+    .WithEnvironment("PERMIFY_DATABASE_URI", postgres.Resource.UriExpression)
+    .WithEnvironment("PERMIFY_SERVICE_WATCH_ENABLED", "true");
 
 builder.AddProject<Permify_Client_AotExample>("aot-example")
     .WithReference(permify.GetEndpoint("http"))
